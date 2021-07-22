ba1fec65f12931f0d926d5e3c39798ff0110a999
unknown <ajhsalejandro@gmail.com>
2021-07-22 15:02:33 -0500

    1.2 Alejandro Herrera

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./Footer";
 import React, { useState, useEffect } from "react";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   useEffect(() => {
     // DEMO: Para obtener los datos de la API en nuestro localhost usar un proxy
     // URL API: https://newsapi.org/v2/everything?q=tesla&from=2021-06-19&sortBy=publishedAt&apiKey=0c76dce6efd947d0bd1f6ac1f4324b9e
+    // NEW URL: https://newsapi.org/v2/everything?q=tesla&from=2021-07-19&apiKey=34d6193c617441eeb152186da0e6f8eb
 
     // const proxyUrl = "https://cors-anywhere.herokuapp.com/"; // URL PROXY (si no te permite por temas de permisos)
     const qInTitle = "tesla"; // noticias sobre "tesla"
@@ -31,19 +33,22 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Lista Noticias de Tesla</h1>
+      <h1 className="h1">Lista Noticias de Tesla</h1>
       <section className="Section">
         {
           listNews.articles &&
           listNews.articles.map((item) => (
-            <section className="Article">
+            <div className="Article">
 
-              <a key={item.publishedAt} href={item.url}>{item.title}</a>
-              <img className="Image" src={item.urlToImage} alt={item.title} />
-            </section>
+              <a key={item.publishedAt} className="a" href={item.url}>{item.title}</a><br />
+              <img key={item.publishedAt} className="Image" src={item.urlToImage} alt={item.title} /><br />
+              <p key={item.publishedAt} className="p">{item.content}</p><br />
+              <p className="foot">{item.author} <br /> {item.publishedAt}</p>
+            </div>
           ))
         }
       </section>
+      <Footer />
     </div>
   );
 }
