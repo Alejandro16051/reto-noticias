eb4d50f739b5ba2fff1c66a2073a46f1f5758c7e
Edenmaru <eden_maru1@outlook.com>
2021-07-21 20:21:06 -0500

    Componente cabecera

---
+++ src/components/Cabecera.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Cabecera() {
+  return (
+    <header>
+      <div>
+        <p>Logo</p>
+        <button>
+          <a href="">Ingresar</a>
+        </button>
+        <button>
+          <a href="">Registrarme</a>
+        </button>
+      </div>
+    </header>
+  );
+}
