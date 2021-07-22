87eae76c04d0033290402911f5e2247dc19c49f6
Edenmaru <eden_maru1@outlook.com>
2021-07-22 18:13:36 -0500

    encabezado responsive

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Hamburger from "hamburger-react";
 
 export default function Cabecera() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <header className="Cabecera">
       <h1 className="TituloPrincipal">Logo</h1>
-      <nav className="NavegacionLogin">
+      <nav className={click ? "NavegacionLoginActiva" : "NavegacionLogin"}>
         <button id="ingresar">Ingresar</button>
         <button id="registrarme">Registrarme</button>
       </nav>
+      <div className="HamburgerDiv" onClick={handleClick}>
+        <Hamburger id="Hamburger"></Hamburger>
+      </div>
     </header>
   );
 }
